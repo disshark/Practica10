28439584dd9b4d35690b6a994da9c1845b5edeba
OsleepO <jwz179410@gmail.com>
2024-04-15 21:36:09 +0200

    save

--- src/P2_Colaborativa_Equipo4_Herencia/Entrenador.java
+++ src/P2_Colaborativa_Equipo4_Herencia/Entrenador.java
@@ -1,7 +1,16 @@
 package P2_Colaborativa_Equipo4_Herencia;
 
+import java.util.ArrayList;
+
 public class Entrenador extends Miembro{
     public Entrenador(String nombre, String cargo, String equipo) {
         super(nombre, cargo, equipo);
+    }
+
+    public static ArrayList<Jugador> jugadorTitular() {
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+
+
+        return jugadores;
     }
 }
