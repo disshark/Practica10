debb10aa91565f6ce70463ff60a1ef747b93c5dc
jousso <davidblazquezlarrinzar@gmail.com>
2024-04-15 16:43:13 +0200

    save

--- src/P2_Colaborativa_Equipo4_Herencia/Main.java
+++ src/P2_Colaborativa_Equipo4_Herencia/Main.java
@@ -1,11 +1,8 @@
 package P2_Colaborativa_Equipo4_Herencia;
 
-import java.io.BufferedReader;
 
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,18 +10,19 @@ import java.util.List;
 public class Main {
     private static List<String> clubes = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
-
-
+        cargarDts();
     }
+
     public  static void cargarDts() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(new File("NavesLigas.txt")));
+        BufferedReader br = new BufferedReader(new FileReader(new File("NavesLigasData.txt")));
         String line = "";
         while ((line = br.readLine()) != null) {
             String[] values = line.split(";");
-
+            System.out.println(line);
         }
         br.close();
+
     }
 }
