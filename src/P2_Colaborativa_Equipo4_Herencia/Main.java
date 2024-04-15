529a621b24f31942aab579c111f3cda3c5ff6297
OsleepO <jwz179410@gmail.com>
2024-04-15 16:03:32 +0200

    save

--- src/P2_Colaborativa_Equipo4_Herencia/Main.java
+++ src/P2_Colaborativa_Equipo4_Herencia/Main.java
@@ -1,24 +1,30 @@
 package P2_Colaborativa_Equipo4_Herencia;
 
-    }
+import java.io.BufferedReader;
 
 
-public  static void cargarDts(){
-    BufferedReader br = new BufferedReader(new FileReader(new File("NavesLigas.txt")));
-    String line = "";
-    while ((line = br.readLine()) != null) {
-        String[] values = line.split(";");
-
-    }
-    br.close();
-}import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     private static List<String> clubes = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello World");
 
 
+    }
+    public  static void cargarDts() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File("NavesLigas.txt")));
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(";");
+
+        }
+        br.close();
+    }
 }
